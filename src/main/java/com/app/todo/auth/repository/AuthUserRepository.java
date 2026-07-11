93475dfe8da0c6322f3c4b5ac3190c5ab1a0795a
Kiran Ganji <kiranganji2003@gmail.com>
2026-07-11 20:07:10 +0530

    added annotation

--- src/main/java/com/app/todo/auth/repository/AuthUserRepository.java
+++ src/main/java/com/app/todo/auth/repository/AuthUserRepository.java
@@ -2,10 +2,12 @@ package com.app.todo.auth.repository;
 
 import com.app.todo.auth.entity.AuthUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface AuthUserRepository extends JpaRepository<AuthUser, UUID> {
     Optional<AuthUser> findByEmail(String email);
 }
